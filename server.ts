3fdbf9d582551abd5b2d70fff532961436063867
Bao <toibaonguyen@gmail.com>
2024-01-30 23:55:58 +0700

    Initial source

---
+++ server.ts
@@ -0,0 +1,8 @@
+import app from "./src/app";
+
+const PORT=3888;
+
+app.listen(PORT,()=>{
+    console.log("Server is listening on port:",PORT);
+})
+
---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import express, { Request, Response } from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import compression from 'compression';
+
+const app = express();
+
+//init middleware
+app.use(morgan("dev"))
+app.use(helmet())
+app.use(compression())
+
+//init DB
+
+//init router
+
+//handle error
+
+
+export default app;
